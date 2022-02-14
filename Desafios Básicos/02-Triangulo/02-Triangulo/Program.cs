bc087a27b3c487e4381d669e715d7ae8705c3d16
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2022-02-14 10:45:26 -0300

    Commit Desafios básicos C#

---
+++ Desafios Básicos/02-Triangulo/02-Triangulo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace _02_Triangulo
+{
+    class minhaClasse
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
+                Perimetro = XX.X
+                Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
+                Area = XX.X
+
+                ENTRADA
+                A entrada contém três valores reais.
+                
+                SAÍDA
+                O resultado deve ser apresentado com uma casa decimal.
+                
+                Exemplo de Entrada	Exemplo de Saída
+                6.0 4.0 2.0
+                
+                Area = 10.0
+             */
+
+            double a, b, c, resultado;
+            string[] valor = Console.ReadLine().Split();
+            a = Convert.ToDouble(valor[0]);
+            b = Convert.ToDouble(valor[1]);
+            c = Convert.ToDouble(valor[2]);
+
+            if ((a <(b + c)) && (b < (a + c)) && (c < (a + b))) //complete a condicional
+            {
+                resultado = a + b + c;
+                Console.WriteLine("Perimetro = {0:0.0}", resultado.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                resultado = ((a + b) * c) / 2;
+                Console.WriteLine("Area = {0:0.0}", resultado.ToString("F1", CultureInfo.InvariantCulture));
+            }   
+        }
+    }
+}
---
+++ Desafios Básicos/03 - ComprasNoSupermercado/ComprasNoSupermercado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComprasNoSupermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+            List<string> listaCompra;
+            List<string> listaSemDuplicidade;
+
+
+            for (int i = 0; i < totalDeCasosDeTeste; i++)
+            {
+                listaCompra = new List<String>(Console.ReadLine().Split(' '));
+                listaSemDuplicidade = listaCompra.Distinct().ToList();
+
+                listaSemDuplicidade.Sort();
+
+                foreach (string lista in listaSemDuplicidade)
+                {
+                    Console.Write($"{lista} ");
+                }
+
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
