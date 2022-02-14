a1c27d21aa3f6b27b772628032ac73e3025ffa71
glauber-cm <54870054+glauber-cm@users.noreply.github.com>
2022-02-14 10:41:16 -0300

    Commit Desafios Iniciais C#

---
+++ Desafios Iniciais/02- QuadradoeaoCubo/QuadradoeaoCubo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QuadradoeaoCubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine($"{i} {Math.Pow(i,2)} {Math.Pow(i, 3)}"); 
+
+            }
+        }
+    }
+}
---
+++ Desafios Iniciais/03- SomaParesConsecutivos/SomaParesConsecutivos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SomaParesConsecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            while ((x = int.Parse(Console.ReadLine())) != 0)
+            {
+                int soma = 0;
+
+                if (x % 2 != 0)
+                {
+                    x++;
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    soma += x + (i * 2);
+                }
+                Console.WriteLine($"{soma}");
+            }
+
+        }
+    }
+}
